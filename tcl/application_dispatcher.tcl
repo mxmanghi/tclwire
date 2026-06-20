00f87b9ea2f74818ffcbcb458d732f68d3f3fcb9
Massimo Manghi <mxmanghi@apache.org>
2026-06-20 09:24:31 +0200

    costructor arguments reformatting

--- tcl/application_dispatcher.tcl
+++ tcl/application_dispatcher.tcl
@@ -210,10 +210,10 @@ oo::class create ::tclwire::ApplicationDispatcher {
         set exit_pool_notification {}
         if {$pool_key ne {}} {
             set exit_pool_notification [format {
-            catch {::tclwire::tpba request [dict create \
-                operation remove_worker \
-                pool_key %s \
-                worker_id [::thread::id]]}
+                catch {::tclwire::tpba request \
+                        [dict create operation remove_worker \
+                                     pool_key  %s \
+                                     worker_id [::thread::id]]}
             } [list $pool_key]]
         }
         return [format {
